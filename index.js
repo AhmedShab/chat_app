f112d9a11d99c6b9a44e46416963e70edefdd0e3
Ahmed Shaaban <ahmed.vuw@gmail.com>
2016-04-11 15:20:41 +1200

    chat is running both in the front and backend

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname +'/public'));
+
+io.on('connection', function(socket){
+  console.log("new user is has joined in");
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
 http.listen(port, function () {
   console.log('Server is starting at port ' + port);
